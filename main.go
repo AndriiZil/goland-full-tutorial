86a0f90bf0716a8e12514d9c5c46b92db5c7be50
Andrii Zilnyk <andrii.zilnyk@eleks.com>
2022-03-08 17:23:58 +0200

    scan, pointer

--- main.go
+++ main.go
@@ -3,11 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference" // Changable
-	const conferenceTickets = 50         // Constant
-	var remainingTickets = 50
+	conferenceName := "Go Conference" // Changable
+	const conferenceTickets int = 50  // Constant
+	var remainingTickets uint = 50
+
+	// remainingTickets = -1 // Error
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
 	fmt.Printf("Wellcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+	// ask user for their name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName) // Pointer
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	// fmt.Println(remainingTickets)
+	// fmt.Println(&remainingTickets)
+
+	userTickets = 2
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 }
